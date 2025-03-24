824b1e83d1c8bb0d72650e560883188e92fec1ac
ayoub0030 <ayoub.elmeo@gmail.com>
2025-03-24 23:27:12 +0000

    going to normal

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -14,11 +14,12 @@ function Home2() {
               LET ME <span className="purple"> INTRODUCE </span> MYSELF
             </h1>
             <p className="home-about-body">
-              I fell in love w, I think… 🤷‍♂️
+              I fell in love with programming and I have at least learnt
+              something, I think… 🤷‍♂️
               <br />
-              <br />I am fluent in cla like
+              <br />I am fluent in classics like
               <i>
-                <b className="purple">  Java  </b>
+                <b className="purple"> C++, Javascript and Go. </b>
               </i>
               <br />
               <br />
